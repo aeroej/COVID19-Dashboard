88dfbd09cf0d82d6f51a4a11b826e6ac78a4e0c0
Your Name <you@example.com>
2020-09-01 14:56:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log('hi index');
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+function handleListening(){
+    console.log('Listening on: http://localhost:${PORT}');
+
+}
+
+function handleHome(req, res){
+    res.send("hello");
+}
+
+
+app.get("/", handleHome);
+app.listen(PORT, handleListening);
+
+
+

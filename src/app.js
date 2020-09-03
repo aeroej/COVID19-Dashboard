fc6f596d4786ae8bf6ba3596767ce4662d47a211
Your Name <you@example.com>
2020-09-03 16:21:56 +0900

    Add a template engine : ejs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.listen(3000, function(){
 app.use(express.static('views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
     // res.send("<h1 />hello!")
@@ -28,5 +29,7 @@ app.post('/email_post', function(req, res){
     //get : req.param('email')
     console.log(req.body.email)
     // html 작성 시 쌍따옴표
-    res.send("<h1>welcome " + req.body.email + "</h1>")
+    // res.send("<h1>welcome " + req.body.email + "</h1>")
+    res.render('../templates/email.ejs', {'email' : req.body.email})
 });
+

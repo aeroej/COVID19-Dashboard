342c65698448eb1de1c22abf5b04c36dd9dbdb21
Your Name <you@example.com>
2020-09-04 14:08:39 +0900

    Modify form.js : Ajax

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-app.listen(3000, function(){
-    console.log("start, express server on port 3000");
+app.listen(5000, function(){
+    console.log("start, express server on port 5000");
 });
 
 app.use(express.static('views'))
@@ -11,14 +11,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
+
 app.get('/', function(req, res){
-    // res.send("<h1 />hello!")
+    // res.send("<h1></h1>")
     res.sendFile(__dirname + "/views/index.html")
 });
 
 app.get('/index', function(req, res){
-    // res.send("<h1 />hello!")
     res.sendFile(__dirname + "/views/index.html")
+});
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname + "/views/home.html")
 });
 
 app.get('/email', function(req,res){
@@ -33,3 +37,9 @@ app.post('/email_post', function(req, res){
     res.render('../templates/email.ejs', {'email' : req.body.email})
 });
 
+app.post('/ajax_send_email', function(req, res){
+    console.log(req.body.email);
+    //check validation about input value => select db
+    var responseData = {'result': 'ok', 'email' : req.body.email}
+    res.json(responseData)
+});

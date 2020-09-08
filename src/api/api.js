12adaa3ed270f077888ae573b88592adbb0a5340
Your Name <you@example.com>
2020-09-08 18:08:16 +0900

    Delete button : index.html

--- src/api/api.js
+++ src/api/api.js
@@ -3,28 +3,33 @@
  * 해당 소스를 함수로 만들기
  * 만들어진 함수를 export 해보기
  */
-window.onload = function(){
-    document.getElementById('getData').addEventListener('click', getData);
 
-    function getData(){
-        fetch('https://api.covid19api.com/dayone/country/south-korea/status/confirmed')
-        .then((res)=>res.json())
-        .then((data) => {
-            let output = `
-                <h2>COVID19-DayOne</h2>
+    // button 삭제
+    // document.getElementById('getData').addEventListener('click', getData);
+    
+window.onload = getData();
+
+function getData(){
+    fetch('https://api.covid19api.com/dayone/country/south-korea/status/confirmed')
+    .then((res)=>res.json())
+    .then((data) => {
+        let output = `
+            <h2>COVID19-DayOne</h2>
+        `;
+        data.forEach(function(e){
+            output += `
+            <u1>
+                <li>Country : ${e.Country}</li>
+                <li>CountryCode : ${e.CountryCode}</li>
+                <li>Cases : ${e.Cases}</li>
+                <li>Date : ${e.Date}</li>
+            </u1>      
+            </br>              
             `;
-            data.forEach(function(e){
-                output += `
-                <u1>
-                    <li>Country : ${e.Country}</li>
-                    <li>CountryCode : ${e.CountryCode}</li>
-                    <li>Cases : ${e.Cases}</li>
-                    <li>Date : ${e.Date}</li>
-                </u1>      
-                </br>              
-                `;
-            })
-            document.getElementById('output').innerHTML = output;
-        });
-    }
+        })
+        // return output;
+        document.getElementById('output').innerHTML = output;
+    });
 }
+
+// export default getData;

bf2156f0db8101bc7931f8d1129e4b012ca7445e
Your Name <you@example.com>
2020-09-14 10:00:49 +0900

    Add css style

--- src/api/api.js
+++ src/api/api.js
@@ -10,21 +10,44 @@
 window.onload = getData();
 
 function getData(){
-    fetch('https://api.covid19api.com/dayone/country/south-korea/status/confirmed')
+    fetch('https://api.covid19api.com/dayone/country/south-korea')
     .then((res)=>res.json())
     .then((data) => {
         let output = `
-            <h2>COVID19-DayOne</h2>
+            <div class="container mb-4">
+                <h3 class="display-4 mb-4">Day One All Status</h2>
+                <h5 class="text-monospace">Returns all cases by case type for a country from the first recorded case.
+                </h5>
+            </div>
+            <table class="container" align ="center">
+                <thead>
+                    <tr align ="center">
+                        <th width="200">Country</th>
+                        <th width="200">Date</th>
+                        <th width="100">Confirmed</th>
+                        <th width="100">Recovered</th>
+                        <th width="100">Deaths</th>
+                        <th width="100">Active</th>
+                    </tr>
+                </thead>
+            </table>
+
+            
         `;
         data.forEach(function(e){
             output += `
-            <u1>
-                <li>Country : ${e.Country}</li>
-                <li>CountryCode : ${e.CountryCode}</li>
-                <li>Cases : ${e.Cases}</li>
-                <li>Date : ${e.Date}</li>
-            </u1>      
-            </br>              
+            <table class="container" align ="center">
+                <tbody>
+                    <tr align ="center">
+                        <td width="200">${e.Country}</td>
+                        <td width="200">${e.Date}</td>
+                        <td width="100">${e.Confirmed}</td>
+                        <td width="100">${e.Recovered}</td>
+                        <td width="100">${e.Deaths}</td>
+                        <td width="100">${e.Active}</td>
+                    </tr>
+                </tbody>     
+            </table>           
             `;
         })
         // return output;
--- src/app.js
+++ src/app.js
@@ -6,18 +6,12 @@ app.listen(5000, function(){
     console.log("start, express server on port 5000");
 });
 
-// var data = {
-//     name : "",
-//     addr:"",
-//     tel:""
-// };
-
 app.use(express.static('views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
-
+// 루트페이지
 app.get('/', function(req, res){
     // res.send("<h1></h1>")
     res.sendFile(__dirname + "/views/index.html")
@@ -27,28 +21,26 @@ app.get('/index', function(req, res){
     res.sendFile(__dirname + "/views/index.html")
 });
 
-app.get('/home', function(req, res){
-    res.sendFile(__dirname + "/views/home.html")
-});
-
 app.get('/email', function(req,res){
     res.sendFile(__dirname + "/views/form.html")
 })
 
-app.get('/member', function(req,res){
-    res.sendFile(__dirname + "/views/member.html")
-})
-
-app.get('/fetch', function(req,res){
-    res.sendFile(__dirname + "/views/fetch.html")
-})
+app.post('/member',(req,res)=>{
+    console.log("POST로 데이터 수신");
+    console.log("이름 : " + req.body.name);
+    console.log("전화 : " + req.body.tel);
+    console.log("이메일 : " + req.body.email);
+    res.send("Server Get");
+});
 
 app.post('/email_post', function(req, res){
     //get : req.param('email')
+    console.log(req.body.name)
+    console.log(req.body.tel)
     console.log(req.body.email)
     // html 작성 시 쌍따옴표
     // res.send("<h1>welcome " + req.body.email + "</h1>")
-    res.render('../templates/email.ejs', {'email' : req.body.email})
+    res.render('../templates/email.ejs', {'name' : req.body.name, 'tel' : req.body.tel, 'email' : req.body.email})
 });
 
 app.post('/ajax_send_email', function(req, res){
@@ -58,13 +50,7 @@ app.post('/ajax_send_email', function(req, res){
     res.json(responseData)
 });
 
-app.post('/member',(req,res)=>{
-    console.log("POST로 데이터 수신");
-    console.log("이름 : " + req.body.name);
-    console.log("주소 : " + req.body.addr);
-    console.log("전화 : " + req.body.tel);
-    res.send("Server Get");
-});
+
 
 
 app.get('/api', function(req,res){
